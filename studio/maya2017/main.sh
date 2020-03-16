5a62680c5e1e7b74b423fa5bb7a7bd4609baa53f
subing85 <subing85@gmail.com>
2020-03-16 14:23:34 +0800

    added usd pipe

--- studio/maya2017/main.sh
+++ studio/maya2017/main.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
-export SOURCE_CODE="/venture/source_code/subins_tutorials/"
-export PYTHONPATH=$PYTHONPATH:$SOURCE_CODE
-# export MAYA_PLUG_IN_PATH=$MAYA_PLUG_IN_PATH":/venture/subins_tutorials/crowd/plug-in"
+SOURCE_CODE="/venture/source_code/subins_tutorials/"
+USD_INSTALL_ROOT="/usr/local/usd/maya2017/pixar/19.05"
+USD_PYTHONPATH=$USD_INSTALL_ROOT"/lib/python/"
 
-echo "Maya"
+export PYTHONPATH=$PYTHONPATH:$SOURCE_CODE:$USD_PYTHONPATH
+export MAYA_CM_DISABLE_ERROR_POPUPS=1
+export MAYA_COLOR_MGT_NO_LOGGING=1 
+
+export MAYA_PLUG_IN_PATH=$MAYA_PLUG_IN_PATH:$USD_INSTALL_ROOT/third_party/maya/plugin/
+export MAYA_SCRIPT_PATH=$MAYA_SCRIPT_PATH:$USD_INSTALL_ROOT/third_party/maya/lib/usd/usdMaya/resources/:$USD_INSTALL_ROOT/third_party/maya/plugin/pxrUsdPreviewSurface/resources/ 
+export XBMLANGPATH=$XBMLANGPATH:$USD_INSTALL_ROOT/third_party/maya/lib/usd/usdMaya/resources/ 
+export PATH=$PATH:$USD_INSTALL_ROOT/lib:$USD_INSTALL_ROOT/bin:$USD_INSTALL_ROOT/third_party/maya/lib
+
+echo "Autodesk Maya"
 echo "Release: 2017"
 echo "version: Update-5"
 echo ""
--- studio/test.sh
+++
@@ -1,4 +0,0 @@
-path="/venture/resource/studio/eclipse/main.shs"
-
-if [ -f $path ]; then echo 'Exists'; else echo 'Not found'; fi
-
